41c814d24fb4ce98ed80c47173321400d3a7df0a
felipedevl <4ks7mxrtmj@privaterelay.appleid.com>
2026-04-26 19:12:22 -0300

    initial logic and some exercices

--- src/academy/devdojo/jornadajavadefinitiva/introducao/CircleArea.java
+++ src/academy/devdojo/jornadajavadefinitiva/introducao/CircleArea.java
@@ -1,8 +1,10 @@
 package academy.devdojo.jornadajavadefinitiva.introducao;
 import java.util.Scanner;
+import java.util.Locale;
 
-public class AreaDoCirculo {
+public class CircleArea {
     public static void main(String[] args)  {
+        Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
         double raio = input.nextDouble();
         double pi = 3.14159;
