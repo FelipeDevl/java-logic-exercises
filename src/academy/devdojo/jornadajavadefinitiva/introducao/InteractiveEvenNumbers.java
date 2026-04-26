8b2ca8e1684a27b82c1148c0d22e6d443b662378
felipedevl <felipeosilva02@gmail.com>
2026-04-26 11:44:50 -0300

    Add even numbers class

---
+++ src/academy/devdojo/jornadajavadefinitiva/introducao/InteractiveEvenNumbers.java
@@ -0,0 +1,16 @@
+package academy.devdojo.jornadajavadefinitiva.introducao;
+import java.util.Scanner;
+
+public class InteractiveEvenNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the total quantity of numbers and it will show only the even numbers: ");
+        int quantity = input.nextInt();
+        for (int num = 1; num <= quantity; num++) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+        input.close();
+    }
+}
